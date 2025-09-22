8e6ca979be5a468654c9c3ae08c000e5250ad56f
martinalejandroo25 <martinalejandroo25@gmail.com>
2025-09-22 19:24:18 +0200

    Ficheros python(a completar)

---
+++ cadenas.py
@@ -0,0 +1,12 @@
+texto = 'Hola Mundo'
+texto2 = " Esto de programar en Python no se yo..."
+
+print(texto, texto2)
+print(texto2 + " " + texto)
+
+texto3 = texto2 + " - " + texto
+print(len(texto3))
+
+print("Me gusta 'Python'")
+
+print("Me gusta 'Python' \nEspero aprender mucho")
---
+++ condicional.py
@@ -0,0 +1,10 @@
+password = "12345678"
+password_enc = "12345678"
+
+if password == password_enc:
+    print("Acceso concedido")
+    print("Otra cosa mas")
+else:
+    print("Acceso denegado")
+
+print("Fin del programa")
