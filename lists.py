45ef5ed6bea18257b01341c75195848ae1c27c0c
martinalejandroo25 <martinalejandroo25@gmail.com>
2025-09-23 14:05:56 +0200

    practicas

---
+++ lists.py
@@ -0,0 +1,11 @@
+#creacion
+lista = list() #lista vacia
+lista2 = [] #lista vacia
+lista3 = [1,2,3,4,5]
+lista4 = ['python', 'javascript', 'java', 'c++']
+
+print(len(lista3))
+print(lista2)
+print(lista4)
+print(lista4[3])
+
